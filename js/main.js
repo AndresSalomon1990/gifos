0dff788d9dd2a0b84d87ccee427d9d26270ffe54
AndresSalomon1990 <a.salomon.1990@gmail.com>
2021-03-09 00:11:30 -0300

    Finish index html/css responsive.

--- js/main.js
+++ js/main.js
@@ -19,3 +19,20 @@ import renderGifsModule from "./utils/renderGifs.js";
 //-------------------------------------------------------------
 
 //-----------------------Header listeners----------------------
+// Change style of Navbar on scroll
+
+
+window.onscroll = function() { 
+    if(window.screen.width > 980) {
+        changeTopNavStyle();
+    } };
+    
+const changeTopNavStyle = () => {
+    const topnavContainer = document.getElementById("topnav-container");
+
+    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+        topnavContainer.style.boxShadow = "0 2px 4px 1px rgba(156,175,195,0.55)";
+    } else {
+        topnavContainer.style.removeProperty("box-shadow");
+    }
+}
